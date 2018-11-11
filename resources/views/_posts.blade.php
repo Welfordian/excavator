0cba9ba264410abe380e28b22c73a0efbec8c7fc
welfordian <josh@fish.cat>
2018-11-11 13:55:36 -0500

    Remove user-generated files

--- resources/views/_posts.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        @foreach($posts as $post)
-            <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-        @endforeach
-    </div>
-@endsection
